7fa2415ddce0015a3cbed9dbadcefba8fed09933
Alexandr Chernov <vonrehcxela@gmail.com>
2022-05-10 19:38:29 +0300

    2HW is done

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import {AffairType} from "./HW2";
 
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType // need to fix any
+    deleteAffairCallback: (id:number)=>void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
-
+    const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)}// need to fix
+const style = {
+        width: 400,
+        display:'flex',
+    padding:10,
+    justifyContent: 'space-between',
+    border: '1px solid'
+}
     return (
-        <div>
-            // show some text
-
+        <div style={style}>
+           <div>{props.affair.name}</div>
+           <div>{props.affair.priority}</div>
             <button onClick={deleteCallback}>X</button>
         </div>
     )
